18241d9cda77f270ce47e8106c99b2cd89de4dc1
KenjiNakamura <kenji.nkmr.1117@gmail.com>
2021-01-03 16:17:46 +0900

    create image upload

--- src/App.js
+++ src/App.js
@@ -24,15 +24,10 @@ const dataProvider = jsonServerProvider(endpoint, httpClient);
 
 const myDataProvider = {
   ...dataProvider,
-  update: (resource, params) => {
+  create: (resource, params) => {
     if (resource !== "articles" || !params.data.pictures) {
-      // fallback to the default implementation
-      return dataProvider.update(resource, params);
+      return dataProvider.create(resource, params);
     }
-    /**
-     * For posts update only, convert uploaded image in base 64 and attach it to
-     * the `picture` sent property, with `src` and `title` attributes.
-     */
 
     // Freshly dropped pictures are File objects and must be converted to base64 strings
     const newPictures = params.data.pictures.filter(
@@ -42,7 +37,35 @@ const myDataProvider = {
       (p) => !(p.rawFile instanceof File)
     );
 
-    // const newPictures = params.data.pictures;
+    return Promise.all(newPictures.map(convertFileToBase64))
+      .then((base64Pictures) =>
+        base64Pictures.map((picture64) => ({
+          src: picture64,
+          title: `${params.data.title}`,
+        }))
+      )
+      .then((transformedNewPictures) =>
+        dataProvider.create(resource, {
+          ...params,
+          data: {
+            ...params.data,
+            pictures: [...transformedNewPictures, ...formerPictures],
+          },
+        })
+      );
+  },
+  update: (resource, params) => {
+    if (resource !== "articles" || !params.data.pictures) {
+      return dataProvider.update(resource, params);
+    }
+
+    // Freshly dropped pictures are File objects and must be converted to base64 strings
+    const newPictures = params.data.pictures.filter(
+      (p) => p.rawFile instanceof File
+    );
+    const formerPictures = params.data.pictures.filter(
+      (p) => !(p.rawFile instanceof File)
+    );
 
     return Promise.all(newPictures.map(convertFileToBase64))
       .then((base64Pictures) =>
